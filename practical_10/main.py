d3d5a21add5079854a4e3d2f78aea17c4f052f2e
anurag9601 <anuragmishrap13@gmail.com>
2025-03-24 17:06:48 +0530

    upload practical_10

---
+++ practical_10/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/armstrong/<int:n>")
+def get_armstrong(n):
+    copy_n = n
+    sum = 0
+    digits = len(str(copy_n))
+
+    while(n > 0):
+        last_d = n % 10
+        n = n//10
+        sum += last_d**digits
+
+    if(sum == copy_n):
+        result = {
+            "number": copy_n,
+            "is_armstrong": True
+        }
+    else:
+        result = {
+            "number": copy_n,
+            "is_armstrong": False
+        }
+    
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- practical_9/main.py
+++ practical_9/main.py
@@ -4,15 +4,23 @@ from flask import Flask , jsonify
 app = Flask(__name__)
 
 #Create route for taking number form params to return the square of it
-@app.route("/square/<int:n>")
+@app.route("/square/<n>")
 
 #Square function
 def square(n):
-    return_data = {
-        "number": n,
-        "square_of_number": n**2
-    }
-    return jsonify(return_data)
+    try:
+        n = int(n)
+        return_data = {
+            "number": n,
+            "square_of_number": n**2
+        }
+        return jsonify(return_data)
+    except:
+        return_data = {
+            "input": n,
+            "error": "Not a number"
+        }
+        return jsonify(return_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
